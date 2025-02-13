100edb97baab756018db02c2d7191daad6a636fa
Jefferson <jeramirezca@unal.edu.co>
2025-02-12 20:43:49 -0500

    push2

---
+++ src/app/usuario/admin/(notificaciones)/page.tsx
@@ -0,0 +1,10 @@
+import Card from "@/components/UI/card";
+
+export default function verNotificaciones() {
+
+  return (
+    <Card aditionalClasses="max-w-3xl">
+      Notificaciones
+    </Card>
+  );
+}
